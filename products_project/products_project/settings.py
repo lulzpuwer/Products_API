308b854779b00c2c12fee814710cc44b34526644
Lulzpower <Zaddicus1@gmail.com>
2022-03-31 14:43:45 -0600

    started products app + migrated tables to the products_db

--- products_project/products_project/settings.py
+++ products_project/products_project/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'products_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'products_db',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'password',
+
     }
 }
 
